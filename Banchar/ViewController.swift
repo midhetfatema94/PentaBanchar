98b188197f0e54185c6e97e18c73cb17baa0e7d7
Midhet Sulemani <midhetfatema94@gmail.com>
2020-03-26 20:16:54 +0300

    Initial Commit

---
+++ Banchar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Banchar
+//
+//  Created by Midhet Sulemani on 3/26/20.
+//  Copyright © 2020 Penta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
