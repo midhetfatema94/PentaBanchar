0654529bec6c9c997087c6366b298d7f3b974408
Midhet Sulemani <midhetfatema94@gmail.com>
2020-04-28 14:35:57 +0300

    removed Google Plist

--- Banchar/ViewModels/LoginViewModel.swift
+++ Banchar/ViewModels/LoginViewModel.swift
@@ -70,37 +70,20 @@ class LoginViewModel {
                 }
             }
         })
-//        let docRef = db.collection("orders").document(userCredentials["userId"] as? String ?? "")
-//        docRef.getDocument(completion: {(document, err) in
-//            if let err = err {
-//                print("Error getting documents: \(err)")
-//                completion(nil)
-//            } else {
-//                if let document = document, document.exists {
-//                    let dataDescription = document.data().map { RequestViewModel(data: RequestOrder(details: $0)) }
-//                    print("Document data: \(dataDescription)")
-//                }
-//            }
-//        })
     }
     
     func getUserDetails(userId: String, completionHandler: @escaping ((Any?) -> Void)) {
-        db.collection("users").getDocuments(completion: {(querySnapshot, err) in
+        let userDocRef = db.collection("users").document(userId)
+        userDocRef.getDocument(completion: {(document, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
                 completionHandler(err.localizedDescription)
             } else {
-                if let snapshot = querySnapshot {
-                    for document in snapshot.documents {
-                        print("\(document.documentID) => \(document.data())")
-                        var userData = document.data()
-                        if document.documentID == userId {
-                            userData["userId"] = document.documentID
-                            completionHandler(userData)
-                        } else {
-                            completionHandler(nil)
-                        }
-                    }
+                if let doc = document, doc.exists {
+                  let dataDescription = doc.data()
+                  print("Cached document data: \(dataDescription)")
+                } else {
+                  print("Document does not exist in cache")
                 }
             }
         })
