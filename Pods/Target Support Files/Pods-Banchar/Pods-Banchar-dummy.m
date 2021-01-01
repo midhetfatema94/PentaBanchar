f113c4bce208e35e7ab5a378a4ccb1bbed6a790d
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-02 02:24:30 +0300

    improved font style

---
+++ Pods/Target Support Files/Pods-Banchar/Pods-Banchar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Banchar : NSObject
+@end
+@implementation PodsDummy_Pods_Banchar
+@end
---
+++ Pods/Target Support Files/Pods-BancharTests/Pods-BancharTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BancharTests : NSObject
+@end
+@implementation PodsDummy_Pods_BancharTests
+@end
